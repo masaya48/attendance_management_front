7c687f4d229cffa8ce143dbefd1c04ca77efe75e
fukazawa <fukazawa@infordio.co.jp>
2018-04-27 16:13:44 +0900

    fix: ログインフローの値渡し修正

--- src/renderer/components/pages/Login.vue
+++ src/renderer/components/pages/Login.vue
@@ -2,7 +2,7 @@
   <section>
     <input type="text" v-model="id" placeholder="ID">
     <input type="password" v-model="password" placeholder="password">
-    <el-button type="primary" @click="login">Login</el-button>
+    <el-button type="primary" @click="doLogin">Login</el-button>
 
     <div v-if="auth">auth!!!!!!</div>
   </section>
@@ -30,17 +30,17 @@ export default {
   },
   methods: {
     ...mapActions({
-      login: {
-        type: 'auth/login',
-        payload: {
-          id: this.id,
-          password: this.password
-        }
-      }
+      login: 'auth/login'
     }),
     ...mapMutations({
       //
     }),
+    doLogin () {
+      this.login({
+        id: this.id,
+        password: this.password
+      })
+    },
     logined: function () {
       if (this.auth) {
         console.log('logined')
