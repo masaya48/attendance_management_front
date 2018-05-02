ecc7217226fc18e703510ca7d6332c257d02bd61
masaya48 <msya0408@gmail.com>
2018-05-02 23:32:34 +0900

    feat: fontawesome導入

--- src/renderer/main.js
+++ src/renderer/main.js
@@ -4,6 +4,12 @@ import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
 import locale from 'element-ui/lib/locale/lang/ja'
 
+// fontawesome
+import '@fortawesome/fontawesome-free-webfonts/css/fontawesome.css'
+import '@fortawesome/fontawesome-free-webfonts/css/fa-brands.css'
+import '@fortawesome/fontawesome-free-webfonts/css/fa-regular.css'
+import '@fortawesome/fontawesome-free-webfonts/css/fa-solid.css'
+
 import App from './App'
 import router from './router'
 import store from './store'
