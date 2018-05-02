efcb9e26de8335d2a9c0bef626b40b70e02fb098
masaya48 <msya0408@gmail.com>
2018-05-02 14:39:52 +0900

    feat: 時計追加

--- src/renderer/components/pages/Timecard.vue
+++ src/renderer/components/pages/Timecard.vue
@@ -1,7 +1,7 @@
 <template>
   <!-- please write html -->
   <section>
-    <label>11:11:11</label>
+    <label>現在時刻：{{getNowTime}}</label>
   </section>
 </template>
 
@@ -12,7 +12,21 @@ export default {
   name: 'timecard',
   data () {
     return {
+      nowTime: new Date()
     }
+  },
+  computed: {
+    getNowTime: function () {
+      const hour = `00${this.nowTime.getHours()}`.slice(-2)
+      const minute = `00${this.nowTime.getMinutes()}`.slice(-2)
+      const second = `00${this.nowTime.getSeconds()}`.slice(-2)
+      return `${hour}:${minute}:${second}`
+    }
+  },
+  created () {
+    setInterval(() => {
+      this.nowTime = new Date()
+    }, 1000)
   }
 }
 </script>
