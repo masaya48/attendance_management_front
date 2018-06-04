00ac098e11ae625a1e6eae0efebb4b01b8569333
masaya48 <msya0408@gmail.com>
2018-06-04 09:21:21 +0900

    feat: add token to api header

--- src/renderer/services/http-client.js
+++ src/renderer/services/http-client.js
@@ -1,5 +1,6 @@
 import axios from 'axios'
-// import store from '@/store'
+import * as API from '@/constants/api'
+import KEY from '@/constants/key'
 
 const client = axios.create({
   baseURL: 'http://localhost:3000'
@@ -7,14 +8,17 @@ const client = axios.create({
 
 export default (Vue, { store }) => {
   client.interceptors.request.use((config) => {
+    if (config.url.indexOf(API.NO_TOKEN) !== -1) {
+      // urlがpublic以外token付与
+      const token = localStorage.getItem(KEY.TOKEN)
+      config.headers.Authorization = `Bearer ${token}`
+    }
     return config
   }, (error) => {
-    // TODO intercept something
     return Promise.reject(error)
   })
 
   client.interceptors.response.use((response) => {
-    // TODO intercept something
     return response.data
   }, (error) => {
     // エラー共通処理
