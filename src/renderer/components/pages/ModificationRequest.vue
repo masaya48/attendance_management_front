5c8b3387a51361d3be9b25a8c4ae304a7da04c54
masaya48 <msya0408@gmail.com>
2018-06-06 09:31:58 +0900

    feat: タブ追加

--- src/renderer/components/pages/ModificationRequest.vue
+++ src/renderer/components/pages/ModificationRequest.vue
@@ -1,8 +1,9 @@
 <template>
-  <!-- please write html -->
-  <section>
-    <label>ModificationRequest</label>
-  </section>
+  <el-tabs type="card">
+    <el-tab-pane label="依頼作成">依頼作成</el-tab-pane>
+    <el-tab-pane label="依頼一覧">依頼一覧</el-tab-pane>
+    <el-tab-pane label="投稿依頼">投稿依頼</el-tab-pane>
+  </el-tabs>
 </template>
 
 <script>
